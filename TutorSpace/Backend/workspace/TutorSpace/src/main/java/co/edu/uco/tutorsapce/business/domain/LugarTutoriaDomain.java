75d18ff149adbe2812c6c179ecc4c477816fc3d9
KevinMurcia636 <kevinmurciamo636@gmail.com>
2024-05-29 17:05:16 -0500

    Correccion domain

---
+++ TutorSpace/Backend/workspace/TutorSpace/src/main/java/co/edu/uco/tutorsapce/business/domain/LugarTutoriaDomain.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:42f9c6cef0e5d6b904fbaeb106cc205f1a035b0f105360b3429fee942a53ac03
+size 2038
---
+++ TutorSpace/Backend/workspace/TutorSpace/src/main/java/co/edu/uco/tutorsapce/business/domain/TipoSucursalDomain.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a6c13885415b62a00461fd7826658d4a63742b1fa473bff73029a6a5f6610528
+size 961
